4a9b99d19ad711f85aced8d9c724d09b7c2a42c3
Amir Rajan <ar@amirrajan.net>
2016-03-03 09:24:54 -0600

    added 13 to save

--- client/agile-tree-container.jsx
+++ client/agile-tree-container.jsx
@@ -38,19 +38,25 @@ class TreeNode extends Component {
   saveOrNothing(e) {
     if(e.keyCode == 27 && this.props.text != this.state.text) {
       this.props.save(this.state.text);
+      e.preventDefault();
     } else if(e.keyCode == 219 && e.ctrlKey && this.props.text != this.state.text) {
       this.props.save(this.state.text);
+      e.preventDefault();
     } else if(e.keyCode == 13 && this.props.text != this.state.text) {
       this.props.save(this.state.text);
+      e.preventDefault();
     } else if(e.keyCode == 27) {
       this.setState({ text: this.props.text });
       this.props.cancelEdit();
+      e.preventDefault();
     } else if(e.keyCode == 219 && e.ctrlKey) {
       this.setState({ text: this.props.text });
       this.props.cancelEdit();
+      e.preventDefault();
     } else if(e.keyCode == 13) {
       this.setState({ text: this.props.text });
       this.props.cancelEdit();
+      e.preventDefault();
     }
   }
 
